b0c3aaf9aca1d547cbdb42536b28f45049913545
Tabeek Noor Chowdhury <tabeek.noor@lbsbd.com>
2025-12-15 15:20:30 +0600

    Config: Added homepage and deployment scripts for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/namaz-time-app/'
+  // 🛑 Must match the repository name exactly!
+  base: '/Waqt-Tracker/'
 })
